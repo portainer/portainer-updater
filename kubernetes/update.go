401a288e9363fa95ab73d7541ebccea47ba7d3d7
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-02-12 06:15:07 +0530

    fix(kube): timeout check deployment

--- kubernetes/update.go
+++ kubernetes/update.go
@@ -4,16 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"github.com/pkg/errors"
-	"github.com/portainer/portainer-updater/utils"
 	"github.com/rs/zerolog/log"
 	appV1 "k8s.io/api/apps/v1"
 	coreV1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 )
 
 type (
@@ -67,50 +66,17 @@ func Update(ctx context.Context, cli *kubernetes.Clientset, imageName string, de
 	if err != nil {
 		return errors.WithMessage(err, "unable to patch deployment")
 	}
-	err = utils.WaitUntil(ctx, func() bool {
-		log.Debug().
-			Str("deploymentName", newDeployment.Name).
-			Msg("Waiting for deployment update to complete")
 
-		i, err := deployCli.Watch(ctx, metaV1.ListOptions{
-			FieldSelector: fmt.Sprintf("metadata.id=%s", newDeployment.UID),
-		})
-		if err != nil {
-			log.Err(err).
-				Str("deploymentName", newDeployment.Name).
-				Msg("Unable to watch deployments")
+	log.Debug().
+		Str("deploymentName", newDeployment.Name).
+		Msg("Waiting for deployment update to complete")
 
-			return false
-		}
-
-		for event := range i.ResultChan() {
-			deployment, ok := event.Object.(*appV1.Deployment)
-			if !ok {
-				continue
-			}
-
-			for _, condition := range deployment.Status.Conditions {
-				if condition.Type == appV1.DeploymentReplicaFailure && condition.Status == coreV1.ConditionTrue {
-					log.Error().
-						Str("deploymentName", newDeployment.Name).
-						Str("reason", condition.Message).
-						Msg("Unable to wait for deployment update to complete")
-					return false
-				}
-
-				if condition.Type == appV1.DeploymentAvailable && condition.Status == coreV1.ConditionTrue {
-					return true
-				}
-			}
-		}
-
-		return false
-	}, 1*time.Minute, 5*time.Second)
-
+	err = waitForDeployment(ctx, deployCli, newDeployment)
 	if err != nil {
 		log.Err(err).
 			Str("deploymentName", deployment.Name).
 			Msg("Unable to wait for deployment update to complete")
+
 		return errUpdateFailure
 	}
 
@@ -120,4 +86,52 @@ func Update(ctx context.Context, cli *kubernetes.Clientset, imageName string, de
 		Msg("Update process completed")
 
 	return nil
+}
+
+func waitForDeployment(ctx context.Context, deployCli v1.DeploymentInterface, newDeployment *appV1.Deployment) error {
+	timeoutSeconds := int64(30)
+	watcher, err := deployCli.Watch(ctx, metaV1.ListOptions{
+		FieldSelector:  fmt.Sprintf("metadata.name=%s", newDeployment.Name),
+		TimeoutSeconds: &timeoutSeconds,
+	})
+	if err != nil {
+		log.Err(err).
+			Str("deploymentName", newDeployment.Name).
+			Str("deploymentUID", string(newDeployment.UID)).
+			Msg("Unable to watch deployments")
+
+		return errors.WithMessage(err, "unable to watch deployments")
+	}
+
+	for event := range watcher.ResultChan() {
+		deployment, ok := event.Object.(*appV1.Deployment)
+		if !ok || deployment.UID != newDeployment.UID {
+			continue
+		}
+
+		for _, condition := range deployment.Status.Conditions {
+			if condition.Type == appV1.DeploymentReplicaFailure && condition.Status == coreV1.ConditionTrue {
+				log.Error().
+					Str("deploymentName", newDeployment.Name).
+					Str("reason", condition.Message).
+					Msg("Deployment replica failure")
+				return errors.New("deployment replica failure")
+			}
+		}
+
+		log.Debug().
+			Int32("ReadyReplicas", deployment.Status.ReadyReplicas).
+			Int32("AvailableReplicas", deployment.Status.AvailableReplicas).
+			Int32("Replicas", deployment.Status.Replicas).
+			Int32("UnavailableReplicas", deployment.Status.UnavailableReplicas).
+			Int32("UpdatedReplicas", deployment.Status.UpdatedReplicas).
+			Msg("checking replicas condition")
+
+		if deployment.Status.ReadyReplicas > 1 {
+			return nil
+		}
+	}
+
+	return errors.New("timeout")
+
 }
